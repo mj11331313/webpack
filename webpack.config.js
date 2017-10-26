b178ed4dfcabc126b346632ce0bb21a5dd2f311f
mj11331313 <2238178090@qq.com>
2017-10-26 12:01:29 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+module.exports = {
+    entry: {
+        entry: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        // publicPath:'http://localhost:8081/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // use: ['style-loader', 'css-loader']
+                // use: [
+                //     {
+                //         loader: 'style-loader'
+                //     },{
+                //         loader: 'css-loader'
+                //     }
+                // ]
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                  })
+            },{
+                test: /\.(png|jpg|gif)/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 500,
+                        outputPath: 'images/'
+                    }
+                }]
+            },{//打包html中的图片(img标签)
+                test: /\.(html|htm)$/i,//i:不区分大小写
+                use: ['html-withimg-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            minify: {
+                removeAttributeQuotes: true
+            },
+            hash: true,
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin("css/index.css"),
+        // new UglifyJSPlugin()
+        
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: '127.0.0.1',
+        port: 8081,
+        compress: true
+    }
+};
