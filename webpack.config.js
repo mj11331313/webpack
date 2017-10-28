508aa8cc4efdbd02ed0be8f2e8646cd730c40080
mj1331313 <author@2238178090@qq.com>
2017-10-28 11:07:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        // publicPath:'http://localhost:8081/'
+        publicPath:'http://127.0.0.1:8081/'
     },
     module: {
         rules: [
